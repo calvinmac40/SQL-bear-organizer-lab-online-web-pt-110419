75d41b5968ba7c2bc7b6d317d401f9340cb205a2
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-29 15:59:19 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES 
+("Mr. Chocolate",7,"male","black","Calm",1),
+("Rowdy",20,"Male","Black","Easy going",1),
+("Tabitha", 1, "Female","Black","nice",1),
+("Sergeant Brown",19,"Male","Brown","Calm",1),
+("Melissa",13,"Female","Black","Aggressive",0),
+("Grinch",17,"Male","Brown","Angry",1),
+("Wendy",24,"Female", "Black","Overly Aggressive",1),
+(NULL,15,"Male","Black", "Aggressive",1)
